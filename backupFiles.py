8a92cf8485b9653554c185bb0800884b31e95319
Bill Harding <bill@tahzoo.com>
2015-09-22 20:21:21 -0700

    Deleted stuff that isn't needed.

--- backupFiles.py
+++ backupFiles.py
@@ -12,38 +12,9 @@ conn = S3Connection(my_key, my_secret)
 mybucket = conn.get_bucket('williamjeffreyharding.com')
 #mybucket.list()
 
-local_path = "C:\Users\Bill\Documents\Invisible-College-Seattle"
+local_path = "C:\Users\Bill\Documents\Invisible-College-Seattle\{}"
 
-
-
-
-def post_main_web_files(web_path, mybucket):
-	'''
-	post_main_web_files(web_path, mybucket)
-	'''
-	bucket_keys = mybucket.list()
-	file_list = [n for n in os.listdir(web_path) if "." in n]
-	folder_list = [n for n in os.listdir(web_path) if "." not in n]
-	for file in file_list:
-		print file
-		if mybucket.lookup(file) != None:
-			possible_key = mybucket.get_key(file)
-			possible_key.set_contents_from_string(open(web_path + "\\" + file).read())
-	print len(file_list), "files updated"
-
-
-def post_web_files(web_path, folder, mybucket):
-	web_path = web_path + "\\" + folder
-	file_list = [n for n in os.listdir(web_path) if ".ini" not in n]
-	for file in file_list:
-		print file
-		if mybucket.lookup(folder + "/" + file) != None:
-			possible_key = mybucket.get_key(folder + "/" + file)
-			possible_key.set_contents_from_string(open(web_path + "\\" + file).read())
-	print len(file_list), "files updated"
-
-#post one article
-file = "dev_test/IC_Meetup.html"
+file = "dev_test/sample_page.html"
 possible_key = mybucket.get_key(file)
-#then you can just upload one file at a time
-possible_key.set_contents_from_string(open(web_path = r'C:\Users\Bill\Google Drive\Website\Website Resources\dataprocess\keen_log_test.html').read())
+		
+possible_key.set_contents_from_string(open(local_path.format("sample_page.html")).read())
